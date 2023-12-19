7d1d62ecd542f36b67ac8dd05799514a63ee6aae
Hemant Arya <hemantarya194@gmail.com>
2023-12-20 01:04:16 +0530

    small changes in font

--- client/src/components/Ourteammates/team.tsx
+++ client/src/components/Ourteammates/team.tsx
@@ -9,7 +9,7 @@ export const Team = () => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
         <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Our team
+            Our Team
           </h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
             Where Creativity Meets Collaboration , Strength in Diversity, Unity
